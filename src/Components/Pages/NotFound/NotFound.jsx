cc400ba1d484e2542470cbfa09fd9ad5fcb83ba9
camilo3312 <ccvelasquez531@misena.edu.co>
2022-01-25 21:09:47 -0500

    update

---
+++ src/Components/Pages/NotFound/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './NotFound.css'
+
+export const NotFound = () => {
+    return (
+        <main className='main_not_found'>
+            <p className='title_not_found'> <strong>Error 404:</strong> Pagina no encontrada </p>
+            <h1 className='icon_not_found'>:(</h1>
+        </main>
+    )
+};
