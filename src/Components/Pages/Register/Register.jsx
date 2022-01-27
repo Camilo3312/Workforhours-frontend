346534c63cb1ebb7b0ac633820ffa7bbe7e57f8d
camilo3312 <ccvelasquez531@misena.edu.co>
2022-01-27 16:37:25 -0500

    update register

--- src/Components/Pages/Register/Register.jsx
+++ src/Components/Pages/Register/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Button } from '../../IU/Button/Button'
 import { Input } from '../../IU/Input/Input'
+import './Register.css'
 const urlApi = process.env.REACT_APP_API
 
 export const Register = () => {
@@ -10,7 +11,6 @@ export const Register = () => {
     const [fecnac, setFecnac] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [image, setImage] = useState('')
 
     const register = async (e) => {
         e.preventDefault()
@@ -26,7 +26,7 @@ export const Register = () => {
                 fecnac: fecnac,
                 email: email,
                 password: password,
-                imageprofile: image
+                imageprofile: "https://res.cloudinary.com/sena-quindio/image/upload/v1643318160/huxqpdulldadg9ceb4jb.png"
             })
         })
         const resjson = await response.json()
@@ -38,14 +38,21 @@ export const Register = () => {
     }
 
     return (
-        <form onSubmit={register}>
-            <Input funct={setSurnames} type='text' placeholder='Apellidos'/>
-            <Input funct={setNames} type='text' placeholder='Nombres'/>
-            <Input funct={setFecnac} type='date'/>
-            <Input funct={setEmail} type='email' placeholder='Correo'/>
-            <Input funct={setPassword} type='password' placeholder='Contraseña'/>
-            <Input funct={setImage} type='file' />
-            <Button value='Registar'/>
-        </form>
+        <main className='main_register'>
+            <div className="center_main_register">
+                <h1 className='subtitle_register'>Registro</h1>
+                <form className='form_register' onSubmit={register}>
+                    <p className='subtitle_form_register'>Informacion personal</p>
+                    <Input funct={setSurnames} type='text' placeholder='Apellidos'/>
+                    <Input funct={setNames} type='text' placeholder='Nombres'/>
+                    <Input funct={setFecnac} type='date'/>
+
+                    <p className='subtitle_form_register'>Informacion de cuenta</p>
+                    <Input funct={setEmail} type='email' placeholder='Correo'/>
+                    <Input funct={setPassword} type='password' placeholder='Contraseña'/>
+                    <Button value='Registar'/>
+                </form>
+            </div>
+        </main>
     )
 }
