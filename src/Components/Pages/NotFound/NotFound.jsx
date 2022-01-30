59086c34f9b3ec59c81d3b92ec1f9c39f71ca95b
camilo3312 <ccvelasquez531@misena.edu.co>
2022-01-30 14:57:15 -0500

    update

--- src/Components/Pages/NotFound/NotFound.jsx
+++ src/Components/Pages/NotFound/NotFound.jsx
@@ -4,7 +4,7 @@ import './NotFound.css'
 export const NotFound = () => {
     return (
         <main className='main_not_found'>
-            <p className='title_not_found'> <strong>Error 404:</strong> Pagina no encontrada </p>
+            <p className='title_not_found'> <strong>Error 404:</strong> Not found </p>
             <h1 className='icon_not_found'>:(</h1>
         </main>
     )
--- src/Components/Pages/UpdatePassword/UpdatePassword.jsx
+++ src/Components/Pages/UpdatePassword/UpdatePassword.jsx
@@ -1,38 +1,65 @@
 import React, { useState } from 'react'
 import { Button } from '../../IU/Button/Button'
 import { Input } from '../../IU/Input/Input'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
+import './UpdatePassword.css'
 const apiUrl = process.env.REACT_APP_API
 
 export const UpdatePassword = () => {
 
     const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [message, setMessage] = useState('')
+    const [isUpdate, setIsUpdate] = useState(false)
     const params = useParams()
 
     const updatePassword = async (e) => {
         e.preventDefault()
-        const response = await fetch(`${apiUrl}password`,{
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin' : '*',
-                'Authorization': `BEARER ${params.token}`
-            },
-            body: JSON.stringify({
-                emailuser: params.email,
-                password: password
+        if (password != '' && confirmPassword != '' && password === confirmPassword) {
+
+            const response = await fetch(`${apiUrl}password`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Origin': '*',
+                    'Authorization': `BEARER ${params.token}`
+                },
+                body: JSON.stringify({
+                    emailuser: params.email,
+                    password: password
+                })
             })
-        })
-        const resjson = await response.json()
-        console.log(resjson);
+            const resjson = await response.json()
+            if (resjson.value) {
+                setIsUpdate(true)
+            }
+            else {
+                setMessage('A ocurrido un error, intente reenviando un nuevo correo de recuperación')
+            }
+        }
+        else {
+            setMessage('Las contraseñas no coinciden')
+        }
     }
 
     return (
         <main>
-            <form onSubmit={updatePassword}>
-                <Input funct={setPassword} />
-                <Button value='Guardar' />
-            </form>
+            <div className="center_main">
+                {!isUpdate ?
+                    <form className='form_update_password' onSubmit={updatePassword}>
+                        <h1>Nueva contraseña</h1>
+                        <Input funct={setPassword} placeholder='Nueva contraseña' type='password' />
+                        <Input funct={setConfirmPassword} placeholder='Confirmar nueva contraseña' type='password' />
+                        <p>{message}</p>
+                        <Button value='Guardar' />
+                    </form>
+                    :
+                    <div className='flex_is_update'>
+                        <h1>Contraseña actualizada</h1>
+                        <Link to='/login'>ingresar</Link>
+                    </div>
+                }
+            </div>
         </main>
     )
 }
